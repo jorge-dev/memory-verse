57878f72189cf662a4a2f067cbc5a1cc87d44e8d
jorge-dev <jo.luisa94@gmail.com>
2024-12-31 12:09:09 -0700

    feat(TimeCounter): update layout and remove time calculation logic

--- src/components/sections/TimeCounter.tsx
+++ src/components/sections/TimeCounter.tsx
@@ -1,39 +1,26 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-
 interface TimeCounterProps {
   startDate: Date;
 }
 
 export function TimeCounter({ startDate }: TimeCounterProps) {
-  const [timeElapsed, setTimeElapsed] = useState("");
-
-  useEffect(() => {
-    const updateTime = () => {
-      const now = new Date();
-      const diff = now.getTime() - startDate.getTime();
-
-      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-
-      setTimeElapsed(`${days} days, ${hours} hours, ${minutes} minutes`);
-    };
-
-    updateTime();
-    const interval = setInterval(updateTime, 60000); // Update every minute
-
-    return () => clearInterval(interval);
-  }, [startDate]);
-
   return (
     <section className="py-20 bg-white/30 dark:bg-gray-800/30">
       <div className="container mx-auto text-center">
-        <h2 className="text-2xl mb-6">Together Since</h2>
-        <div className="text-4xl font-bold">{timeElapsed}</div>
+        <div>
+          <h2 className="text-4xl font-bold text-white dark:text-gray-100">
+            Time Counter
+          </h2>
+          <p className="text-lg text-gray-300 dark:text-gray-400">
+            This is a simple time counter that shows the time elapsed since the
+            start date.
+          </p>
+          {/* use the date */}
+          <p className="text-6xl font-bold text-white dark:text-gray-100">
+            {startDate.getTime()}
+          </p>
+        </div>
       </div>
     </section>
   );
