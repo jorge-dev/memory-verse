15d8df83beba0a0c427381b279df789cd072b322
jorge-dev <jo.luisa94@gmail.com>
2025-01-07 17:52:10 -0700

    feat(GalleryPage): enhance header with navigation button and improve layout

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,21 +3,26 @@ import Gallery from "@/components/SimpleGallery";
 import { AnimatedSection } from "@/components/AnimatedSection";
 import { images } from "@/data/images";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
 
 export default function GalleryPage() {
   return (
-    <main className="h-screen w-screen overflow-hidden">
+    <main className="flex flex-col min-h-screen">
       <AnimatedSection delay={0.2} className="h-full">
-        <div className="relative pt-4 px-4">
-          <Link
-            href="/"
-            className="absolute left-4 top-4 inline-flex items-center px-4 py-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors duration-200"
-          >
-            ← Back
-          </Link>
-          <h1 className="text-4xl font-bold text-center mb-4">Our Memories</h1>
-        </div>
-        <div className="h-[calc(100vh-5rem)]">
+        <header className="fixed top-0 left-0 right-0 z-10 bg-background/50 backdrop-blur-sm">
+          <div className=" mx-auto pl-1 py-4 flex items-center justify-between">
+            <Button variant="ghost" asChild>
+              <Link href="/" className="flex items-center gap-2">
+                <ArrowLeft className="h-4 w-1" />
+                Back
+              </Link>
+            </Button>
+            <h1 className="text-4xl font-bold">Our Memories</h1>
+            <div className="w-[72px]"></div> {/* Spacer to center the title */}
+          </div>
+        </header>
+        <div className="h-screen  overflow-y-auto mt-20">
           <Gallery items={images} />
         </div>
       </AnimatedSection>
